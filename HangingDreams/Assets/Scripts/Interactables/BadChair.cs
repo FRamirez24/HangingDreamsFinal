ae818dc8a95838cebe8d4ef9e0e4e85c77ccb710
Artur <57915715+arturprokofiev@users.noreply.github.com>
2023-08-07 19:01:59 -0400

    added the final scene

---
+++ HangingDreams/Assets/Scripts/Interactables/BadChair.cs
@@ -0,0 +1,16 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEditor.SceneManagement;
+
+public class BadChair : MonoBehaviour, IInteractable
+{
+    // Start is called before the first frame update
+    public void Interact()
+    {
+        
+    }
+
+    // Update is called once per frame
+
+}
